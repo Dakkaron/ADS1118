9e1a1650bcfff10d12f70cd68bd67ac4e8292fda
Dakkaron <dak1st@gmx.at>
2019-05-22 09:57:26 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+classifiers = ['Development Status :: 5 - Production/Stable',
+               'Operating System :: POSIX :: Linux',
+               'License :: OSI Approved :: MIT License',
+               'Intended Audience :: Developers',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3',
+               'Topic :: Software Development',
+               'Topic :: System :: Hardware']
+
+setup(
+    name='ads1118',
+    version='1.0',
+    author='David Krywult',
+    author_email='dak1st@gmx.at',
+    description="""Python library for the ADS1118 4-channel ADC""",
+    long_description=open('README.md').read(),
+    license='MIT',
+    keywords='Raspberry Pi',
+    classifiers=classifiers,
+    packages=['ads1118'],
+    install_requires=['spidev>=3.4']
+)
